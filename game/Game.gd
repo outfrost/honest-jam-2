8cabd21df4632987eca61dec7bcf4aaa174f60b9
outfrost <kotlet.bahn@gmail.com>
2021-08-07 00:09:48 +0200

    Add level spawning and transition

--- game/Game.gd
+++ game/Game.gd
@@ -1,14 +1,11 @@
-# Copyright 2021 Outfrost
-# This work is free software. It comes without any warranty, to the extent
-# permitted by applicable law. You can redistribute it and/or modify it under
-# the terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See the LICENSE file for more details.
-
 class_name Game
 extends Node
 
+export var level_scene: PackedScene
+
 onready var main_menu: Control = $UI/MainMenu
 onready var transition_screen: TransitionScreen = $UI/TransitionScreen
+onready var level_container: Node = $LevelContainer
 
 var debug: Reference
 
@@ -28,7 +25,19 @@ func _process(delta: float) -> void:
 		back_to_menu()
 
 func on_start_game() -> void:
+	transition_screen.fade_in()
+	yield(transition_screen, "animation_finished")
 	main_menu.hide()
+	var level = level_scene.instance()
+	level_container.add_child(level)
+	transition_screen.fade_out()
 
 func back_to_menu() -> void:
+	transition_screen.fade_in()
+	yield(transition_screen, "animation_finished")
+	var nodes = level_container.get_children()
+	for node in nodes:
+		level_container.remove_child(node)
+		node.queue_free()
 	main_menu.show()
+	transition_screen.fade_out()
