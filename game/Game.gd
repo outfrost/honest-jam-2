4f891ca975e7c7f88d389490bdae4f80fc361213
Ryan <raw9426@gmail.com>
2021-08-08 17:47:46 -0400

    Added day progression calc and win condition checks

--- game/Game.gd
+++ game/Game.gd
@@ -5,6 +5,8 @@ const MUSIC_FADEIN_DURATION: float = 4.0
 const MUSIC_FADEOUT_DURATION: float = 1.0
 
 export var level_scene: PackedScene
+export var day_limt:int = 30
+
 
 onready var music_player: AudioStreamPlayer = $MusicPlayer
 onready var main_menu: Control = $UI/MainMenu
@@ -17,6 +19,17 @@ var debug: Reference
 var fadeMusicIn: bool = false
 var selected_building: Spatial = null
 var hovered_tile: Tile = null
+var level = null
+var day = 0
+
+var power_flow: int = 0
+var water_flow: int = 0
+var climate_flow: int = 0
+var win_climate_flow:int = 500
+var food_flow: int = 0
+var win_food_flow:int = 500
+var minerals_flow: int = 0
+var metal_flow: int = 0
 
 func _ready() -> void:
 	if OS.has_feature("debug"):
@@ -76,7 +89,7 @@ func on_start_game() -> void:
 	transition_screen.fade_in()
 	yield(transition_screen, "animation_finished")
 	main_menu.hide()
-	var level = level_scene.instance()
+	level = level_scene.instance()
 	level_container.add_child(level)
 	music_player.volume_db = linear2db(0.0)
 	music_player.play()
@@ -91,6 +104,14 @@ func back_to_menu() -> void:
 	for node in nodes:
 		level_container.remove_child(node)
 		node.queue_free()
+	level = null
+	power_flow = 0
+	water_flow = 0
+	climate_flow = 0
+	food_flow = 0
+	minerals_flow = 0
+	metal_flow = 0
+	day = 0
 	music_player.stop()
 	main_menu.show()
 	transition_screen.fade_out()
@@ -108,4 +129,48 @@ func tile_under_mouse(pos: Vector2) -> Tile:
 		true)
 	if result.has("collider") && result["collider"].get_parent() is Tile:
 		return result["collider"].get_parent()
+	return null
+
+func update_resources():
+	var start_power_flow:int = power_flow
+	var start_water_flow:int = water_flow
+	var start_climate_flow:int = climate_flow
+	var start_food_flow:int = food_flow
+	var start_minerals_flow:int = minerals_flow
+	var start_metal_flow:int = minerals_flow
+
+	for x in level.tilemap:
+		for z in level.tilemap:
+			if level.tilemap[x][z].building:
+				power_flow += level.tilemap[x][z].building.power_flow
+				water_flow += level.tilemap[x][z].building.water_flow
+				climate_flow += level.tilemap[x][z].building.climate_flow
+				food_flow += level.tilemap[x][z].building.food_flow
+				minerals_flow += level.tilemap[x][z].building.minerals_flow
+				metal_flow += level.tilemap[x][z].building.metal_flow
+
+	var power_flow_diff:int = power_flow - start_power_flow
+	var water_flow_diff:int = water_flow - start_water_flow
+	var clmiate_flow_diff:int = climate_flow - start_climate_flow
+	var food_flow_diff:int = food_flow - start_food_flow
+	var minerals_flow_diff:int = minerals_flow - start_minerals_flow
+	var metal_flow_diff:int = metal_flow - start_metal_flow
+
+	return null
+
+func game_lost():
+
+	return null
+
+func game_won():
+
+	return null
+
+func advance_day():
+
+	update_resources()
+	if power_flow < 0 || water_flow < 0 || climate_flow < 0 || food_flow < 0 || minerals_flow < 0 || metal_flow < 0:
+		game_lost()
+	if day == day_limt && climate_flow >= win_climate_flow && food_flow >= win_food_flow:
+		game_won()
 	return null
