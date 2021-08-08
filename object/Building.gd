0e11ab44a86b6fb8fb7194f30c0f8bb99105f744
outfrost <kotlet.bahn@gmail.com>
2021-08-08 23:05:23 +0200

    Add Building script

---
+++ object/Building.gd
@@ -0,0 +1,12 @@
+extends Spatial
+
+export var power_flow: int = 0
+export var water_flow: int = 0
+export var climate_flow: int = 0
+export var food_flow: int = 0
+export var minerals_flow: int = 0
+export var metal_flow: int = 0
+
+export(Tile.Type) var build_on = Tile.Type.BUILD
+export var requires_neighbour: bool = false
+export(Tile.Type) var neighbour = Tile.Type.BLANK
